175c92b0ac4fd4c04e306d7b1445ad81b3ff1218
GabrielYuSP <e1526978@u.nus.edu>
2026-08-26 16:31:05 +0800

    Add Echo Function

--- src/main/java/Goober.java
+++ src/main/java/Goober.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
+
 public class Goober {
     public static void main(String[] args) {
-        String banner = "____________________________________________________________" +
-                "  ____                  _               \n"
+        String banner = "____________________________________________________________\n" +
+                "  ____              _               \n"
                 + " / ___| ___   ___  | |__   ___  _ __  \n"
                 + "| |  _ / _ \\ / _ \\ | '_ \\ / _ \\| '__| \n"
                 + "| |_| | (_) | (_) || |_) |  __/| |    \n"
                 + " \\____|\\___/ \\___/ |_.__/ \\___||_|    \n"
-                + "Hello! I'm Goober.\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                + "____________________________________________________________\n";
+
+        String line = "____________________________________________________________";
+
         System.out.println(banner);
+        System.out.println("Hello! I'm Goober.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("bye")){
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+            }
+            else{
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
     }
 }
