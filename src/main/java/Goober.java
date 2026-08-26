977430c8a6db7d6f65ba4dcb931b5f315528eea8
GabrielYuSP <e1526978@u.nus.edu>
2026-08-26 17:06:49 +0800

    Add 'Add' and 'List' Function

--- src/main/java/Goober.java
+++ src/main/java/Goober.java
@@ -16,20 +16,40 @@ public class Goober {
         System.out.println("Hello! I'm Goober.");
         System.out.println("What can I do for you?");
         System.out.println(line);
+
         Scanner scanner = new Scanner(System.in);
+        // Array to store up to 100 tasks
+        String[] tasks = new String[100];
+        // Task Counter for loop
+        int taskCount = 0;
+
         while(true){
             String input = scanner.nextLine();
-
+            // Base case, break
             if(input.equalsIgnoreCase("bye")){
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
+                break;
             }
-            else{
+            // List Function
+            else if(input.equals("list")){
                 System.out.println(line);
-                System.out.println(input);
+                // Loop through tasks, taskCount number of times
+                for(int i = 0; i < taskCount; i++){
+                    System.out.println((i+1) + "." + tasks[i]);
+                }
+                System.out.println(line);
+            }
+            // Store input as task into array
+            else{
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(line);
+                System.out.println("added " + input);
                 System.out.println(line);
             }
         }
+        scanner.close();
     }
 }
